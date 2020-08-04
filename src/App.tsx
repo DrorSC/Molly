9139ac952445e7a8dceee619957c3beb95f8b4cb
dror <sametcoh@gmail.com>
2020-08-04 17:53:06 +0300

    adding react-router-dom for home and login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import './App.scss';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Login() {
+  return <h2>Login</h2>;
+}
